4dfc914121da4bf5527d2d4bd6c00acf05e33dd5
kateseshi <145038732+kateseshi@users.noreply.github.com>
2023-09-14 21:59:55 +0600

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,3 @@
+Source code of [clocktab.com](https://www.clocktab.com).
+
+It is ancient code progressively undergoing modernization.
---
+++ tab_app_head_tags.js
@@ -0,0 +1,5 @@
+module.exports.tab_app_head_tags = [
+  '<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">',
+  // Trial Token for Wake Lock API
+  '<meta http-equiv="origin-trial" content="AuBkDWSrveJGM5qjy8FIfMgCl7v4N+TjqRiviv1Smv6marflAUFaHLGAGsaZ2fxfR8WgoFqH5WeAUYrlUd8j7gkAAABSeyJvcmlnaW4iOiJodHRwczovL3d3dy5jbG9ja3RhYi5jb206NDQzIiwiZmVhdHVyZSI6Ildha2VMb2NrIiwiZXhwaXJ5IjoxNTkxMjkyNDI0fQ==">',
+];
